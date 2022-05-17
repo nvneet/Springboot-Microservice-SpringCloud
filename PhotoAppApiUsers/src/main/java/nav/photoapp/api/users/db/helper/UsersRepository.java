91f757c0940b2e3a842b1149909303b2a2d58e55
nvneet <navneet.ism@gmail.com>
2022-05-17 06:47:56 +0530

    Revert "Initial commit"
    This reverts commit 2b5395df3a163bc606407c0ab0ccebcd84be97c3.

--- PhotoAppApiUsers/src/main/java/nav/photoapp/api/users/db/helper/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package nav.photoapp.api.users.db.helper;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersRepository extends CrudRepository<UserEntity, Long> {
-	UserEntity findByEmail (String email);
-}
--- mobile-app-ws/src/main/java/nav/app/userservice/UserService.java
+++
@@ -1,8 +0,0 @@
-package nav.app.userservice;
-
-import nav.app.ui.model.request.UserEntityReq;
-import nav.app.ui.model.response.UserEntityResp;
-
-public interface UserService {
-	UserEntityResp createUser(UserEntityReq user);
-}
